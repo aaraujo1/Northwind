a0039b0094f30be26c254e688c5583eb8139efa4
Andre Araujo <aaraujo1@my.wctc.edu>
2018-12-12 23:08:26 -0600

    chartController

---
+++ Northwind/Models/ChartData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Northwind.Models
+{
+    public class ChartData
+    {
+        public string CompanyName { get; set; }
+        public string PresentWorth { get; set; }
+
+        public ChartData(string name, string worth)
+        {
+            CompanyName = name;
+            PresentWorth = worth;
+        }
+    }
+}
