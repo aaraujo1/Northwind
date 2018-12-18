6b30147dc75aa440b95e61ddf3a96d7801c7eaed
André Araujo <andre.gonzalo.araujo@outlook.com>
2018-12-18 17:41:15 -0600

    Changed Chart Controller

--- Northwind/Controllers/ChartController.cs
+++ Northwind/Controllers/ChartController.cs
@@ -8,63 +8,142 @@ using Northwind.Models;
 using Northwind.DataLayer;
 using System.Data.Entity;
 
-
 namespace Northwind.Controllers
 {
     public class ChartController : Controller
     {
-
-        //[AllowCrossSiteJson]
-
-        public JsonResult ChartData()
+        // GET: Chart
+        public ActionResult CategorySales()
         {
-            List<ChartData> cd = new List<ChartData>()
-            {
-                new ChartData("project A", "81531.946062978"),
-                new ChartData("project B", "67313.916593765"),
-                new ChartData("project C", "92440.723376746")
-            };
-
-            return Json(cd, JsonRequestBehavior.AllowGet);
-        }
-
-
-        public ActionResult SalesByCountry()
-        {
-
             using (Northwnd db = new Northwnd())
             {
-
-                //var dbContext = db.Categories.Include("Products").ToList();
-                //var dbContext = db.Order_Details.Include("Products").Include("Categories").ToList();
-
-                /*var dbContext = db.Categories.Include(x => x.Products.Select(o => o.Order_Details)).ToList();
-                ViewBag.dbContext = dbContext;*/
                 return View(db.Categories.OrderBy(c => c.CategoryName).ToList());
             }
-            
         }
 
-        public JsonResult SalesByCountryData()
+        public JsonResult FilterChart(string CategoryName)
         {
-
             using (Northwnd db = new Northwnd())
             {
+                if (CategoryName == null)
+                {
+                    var categoryInfo =
+                        from p in db.Products
+                        join c in db.Categories on p.CategoryID equals c.CategoryID
+                        join od in db.Order_Details on p.ProductID equals od.ProductID
+                        group new { p, c, od } by new { c.CategoryName } into f
+                        select new
+                        {
+                            Name = f.Key.CategoryName,
+                            Sum = f.Sum(s =>
+                                s.od.Quantity * s.od.UnitPrice * (1 - s.od.Discount))
+                        };  //Sum = (od.UnitPrice * od.Quantity * (1 - od.Discount)) };
 
-                //var dbContext = db.Categories.Include("Products").ToList();
-                //var dbContext = db.Order_Details.Include("Products").Include("Categories").ToList();
+                    var catInfo = categoryInfo.ToList();
 
-                var dbContext = db.Categories.Include(x => x.Products.Select(o => o.Order_Details)).ToList();
-                ViewBag.dbContext = dbContext;
-                return Json(dbContext, JsonRequestBehavior.AllowGet);
+                    return Json(catInfo, JsonRequestBehavior.AllowGet);
+                }
+                else
+                {
+                    var productInfo =
+                        from p in db.Products
+                        join c in db.Categories on p.CategoryID equals c.CategoryID
+                        join od in db.Order_Details on p.ProductID equals od.ProductID
+                        where c.CategoryName == CategoryName
+                        group new { p, c, od } by new { p.ProductName } into f
+                        select new
+                        {
+                            Name = f.Key.ProductName,
+                            Sum = f.Sum(s =>
+    s.od.Quantity * s.od.UnitPrice * (1 - s.od.Discount))
+                        };
+                    var prodInfo = productInfo.ToList();
+                    return Json(prodInfo, JsonRequestBehavior.AllowGet);
+                }
             }
-
         }
 
-
-        public ActionResult CategoryBreakdown()
+        public ActionResult YearlySales()
         {
             return View();
+        }
+
+        public JsonResult YearlySalesData()
+        {
+            using (Northwnd db = new Northwnd())
+            {
+                var yearlySalesInfo =
+                    from o in db.Orders
+                    join od in db.Order_Details on o.OrderID equals od.OrderID
+                    group new { o, od } by new { o.OrderDate.Value.Year } into f
+                    orderby f.Key.Year
+                    select new
+                    {
+                        Year = f.Key.Year,
+                        TotalSales = f.Sum(s => s.od.UnitPrice * s.od.Quantity * (1 - s.od.Discount))
+                         
+                    };
+                var yearSaleInfo = yearlySalesInfo.ToList();
+                return Json(yearSaleInfo, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        public ActionResult YearlySalesByCountry()
+        {
+            using (Northwnd db = new Northwnd())
+            {
+                return View(db.Orders.GroupBy(c => c.OrderDate.Value.Year).OrderBy(c => c.Key).Select(c => c.Key).ToList());
+            }
+        }
+
+        public JsonResult test()
+        {
+            using (Northwnd db = new Northwnd())
+            {
+                var test = db.Orders.GroupBy(c => c.OrderDate.Value.Year).Select(c => c.Key);
+                var test2 = test.ToList();
+                return Json(test2, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        public JsonResult YearlySalesByCountryData(int? year)
+        {
+            using (Northwnd db = new Northwnd())
+            {
+                if (year == null)
+                {
+                    var yearlySalesInfo =
+                        from o in db.Orders
+                        join od in db.Order_Details on o.OrderID equals od.OrderID
+                        group new { o, od } by new { o.ShipCountry } into f
+                        orderby f.Key
+                        select new
+                        {
+                            Country = f.Key.ShipCountry,
+                            TotalSales = f.Sum(s => s.od.UnitPrice * s.od.Quantity * (1 - s.od.Discount))
+
+                        };
+                    var yearSaleInfo = yearlySalesInfo.ToList();
+                    return Json(yearSaleInfo, JsonRequestBehavior.AllowGet);
+                }
+                else
+                {
+                    var yearlySalesInfo =
+                        from o in db.Orders
+                        join od in db.Order_Details on o.OrderID equals od.OrderID
+                        where o.OrderDate.Value.Year == year
+                        group new { o, od } by new { o.ShipCountry } into f
+                        select new
+                        {
+                            Country = f.Key.ShipCountry,
+                            TotalSales = f.Sum(s => s.od.UnitPrice * s.od.Quantity * (1 - s.od.Discount))
+
+                        };
+                    var yearSaleInfo = yearlySalesInfo.ToList();
+                    return Json(yearSaleInfo, JsonRequestBehavior.AllowGet);
+                }
+                
+            }
         }
 
     }
